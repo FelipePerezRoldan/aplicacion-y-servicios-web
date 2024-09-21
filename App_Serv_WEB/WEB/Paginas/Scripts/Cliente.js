f5eaed7fcefd02e4dfc2fb259e783b80a8435348
DESKTOP-7QBV6P2\wolff <wolffangfelipe@gmail.com>
2024-09-20 20:35:36 -0500

    Creacion del Script de Cliente.js y en el script se generó el menú con un jQuery() y se creo la función consultar.

---
+++ App_Serv_WEB/WEB/Paginas/Scripts/Cliente.js
@@ -0,0 +1,28 @@
+﻿jQuery(function () {
+    //Registrar los botones para responder al evento click
+    $("#dvMenu").load("../Paginas/Menu.html")
+});
+
+async function Consultar()
+{
+    let Documento = $("#txtDocumento").val();
+    try {
+        const Resultado = await fetch("http://localhost:59788/api/Clientes/ConsultarDocumento?Documento=" + Docuento,
+            {
+
+                method: "GET",
+                mode: "cors",
+                headers: { "Content-Type": "application/json" }
+            });
+        const Respuesta = await Resultado.json();
+        $("#txtNombre").val(Respuesta.Nombre);
+        $("#txtPrimerApellido").val(Respuesta.PrimerApellido);
+        $("#txtSegundoApellido").val(Respuesta.SegundoApellido);
+        $("#txtEmail").val(Respuesta.Email);
+        $("#txtDireccion").val(Respuesta.Direccion);
+        $("#txtFechaNacimiento").val(Respuesta.FechaNacimiento);
+    }
+    catch (error) {
+        $("#dvMesaje").html(error);
+    }
+}
